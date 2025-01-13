8d21deb99068b7e9595e1dedfad485c4e79e62e8
Kasia263 <131020613+Kasia263@users.noreply.github.com>
2025-01-13 16:05:47 +0000

    new file:   .devcontainer/__pycache__/mqtt_subscriber.cpython-312.pyc 	modified:   .devcontainer/mqtt_subscriber.py 	modified:   .devcontainer/testy.py

--- .devcontainer/testy.py
+++ .devcontainer/testy.py
@@ -1,5 +1,6 @@
 import mqtt_publisher as mp
 import os
+import mqtt_subscriber as ms
 
 
 city = os.getenv("LOCATIONS", "Tokyo")
@@ -10,5 +11,8 @@ MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", "sys-wbud")
 STUDENT_ID = os.getenv("STUDENT_ID", "261334")
 TOPIC = f"{STUDENT_ID}/{city}"
 
+
 mqtt = mp.MQTTPublisher(city,BROKER_ADDRESS,BROKER_PORT,MQTT_USER,MQTT_PASSWORD,STUDENT_ID,TOPIC)
 #mqtt.publish_weather()
+subscriber = ms.MQTTSubscriber(BROKER_ADDRESS, BROKER_PORT, MQTT_USER, MQTT_PASSWORD, STUDENT_ID, TOPIC)
+subscriber.start_subscription()
