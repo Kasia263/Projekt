e360d5fbeb892990a87b905948be5b2262b95ccd
Kasia263 <131020613+Kasia263@users.noreply.github.com>
2025-01-13 15:31:39 +0000

    modified:   .devcontainer/WeatherRequester.py 	modified:   .devcontainer/__pycache__/WeatherRequester.cpython-312.pyc 	modified:   .devcontainer/__pycache__/mqtt_publisher.cpython-312.pyc 	modified:   .devcontainer/mqtt_publisher.py 	modified:   .devcontainer/mqtt_subscriber.py#	modified:   .devcontainer/testy.py

--- .devcontainer/testy.py
+++ .devcontainer/testy.py
@@ -1,7 +1,9 @@
 import WeatherRequester as wr
+import mqtt_publisher as ms
 
 print("Konsolowa aplikacja pogodowa")
 city = input("Podaj nazwę miasta: ")
 weather = wr.WeatherRequester('Warsaw')
+mqtt = ms.MQTTPublisher()
 weather_info = weather.fetch_weather()
 print("\n" + weather_info)
